42827fb90e2be2c154c88dd49328b33ccfbd1cdc
Jonathan CHARDON <jonathan.chardon@skale-5.com>
2017-04-24 19:05:10 +0200

    fix space for ansible managed check

--- ansible_check.py
+++ ansible_check.py
@@ -243,9 +243,9 @@ def check_templates():
          firstline = f.readline()
 
          if not template_filename.endswith(".json.j2"):
-           if ("# {{ansible_managed}}" not in  firstline) and ("// {{ansible_managed}}" not in  firstline):
+           if ("# {{ ansible_managed }}" not in  firstline) and ("// {{ ansible_managed }}" not in  firstline):
               return_code = 2
-              print "first line of template file {} need to be # {{{{ansible_managed}}}} or {{{{ansible_managed}}}}".format(full_template_path)
+              print "first line of template file {} need to be # {{{{ ansible_managed }}}} or {{{{ ansible_managed }}}}".format(full_template_path)
 
        flag=False
        with open(full_template_path ,"r") as f:
