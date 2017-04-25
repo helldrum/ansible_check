d5d283ac0a5129cae49cfc91d6fa3985743de7cf
Jonathan CHARDON <jonathan.chardon@skale-5.com>
2017-04-25 21:30:56 +0200

    ful path for all file into error messages

--- ansible_check.py
+++ ansible_check.py
@@ -54,7 +54,7 @@ def check_meta_main():
   global role_name
   global role_platform
 
-  meta_file_path = "{}/{}".format(role_path, "/meta/main.yml") 
+  meta_file_path = "{}/{}".format(role_path, "meta/main.yml") 
 
   try:
     meta=yaml_load(meta_file_path)
@@ -68,54 +68,53 @@ def check_meta_main():
     meta=yaml_load(meta_file_path)
     role_platform=meta["galaxy_info"]["platforms"][0]["name"]
   except (IOError, KeyError, TypeError) as e:
-    print """ERROR: some tests depend of the property [\"galaxy_info\"][\"platforms\"][0][\"name\"]
-      into {} please create this propertie""".format(meta_file_path)
+    print "ERROR: some tests depend of the property [\"galaxy_info\"][\"platforms\"][0][\"name\"], it's not exist into {}".format(meta_file_path)
     print "Now i'am sad :("
     sys.exit(2)
 
   try:
     meta["galaxy_info"]["author"]
     if not meta["galaxy_info"]["author"]:
-      print "the key [\"galaxy_info\"][\"author\"] is empty into meta/main.yml"
+      print "the key [\"galaxy_info\"][\"author\"] is empty into {}".format(meta_file_path)
       return_code = 2
   except KeyError as e:
-    print "the key [\"galaxy_info\"][\"author\"] is missing into meta/main.yml"
+    print "the key [\"galaxy_info\"][\"author\"] is missing into {}".format(meta_file_path)
     return_code = 2
 
   try:
     meta["galaxy_info"]["description"]
     if not meta["galaxy_info"]["description"]:
-      print "the key [\"galaxy_info\"][\"description\"] is empty into meta/main.yml"
+      print "the key [\"galaxy_info\"][\"description\"] is empty into {}".format(meta_file_path)
       return_code = 2
   except KeyError as e:
-    print "the key [\"galaxy_info\"][\"description\"] is missing into meta/main.yml"
+    print "the key [\"galaxy_info\"][\"description\"] is missing into {}".format(meta_file_path)
     return_code = 2
 
   try:
     meta["galaxy_info"]["company"]
     if not meta["galaxy_info"]["company"]:
-      print "the key [\"galaxy_info\"][\"company\"] is empty into meta/main.yml"
+      print "the key [\"galaxy_info\"][\"company\"] is empty into {}".format(meta_file_path)
       return_code = 2
   except KeyError as e:
-    print "the key [\"galaxy_info\"][\"company\"] is missing into meta/main.yml"
+    print "the key [\"galaxy_info\"][\"company\"] is missing into {}".format(meta_file_path)
     return_code = 2
 
   try:
     meta["galaxy_info"]["license"]
     if not meta["galaxy_info"]["license"]:
-      print "the key [\"galaxy_info\"][\"licence\"] is empty into meta/main.yml"
+      print "the key [\"galaxy_info\"][\"licence\"] is empty into {}".format(meta_file_path)
       return_code = 2
   except KeyError as e:
-    print "the key [\"galaxy_info/\"][\"license\"] is missing into meta/main.yml"
+    print "the key [\"galaxy_info/\"][\"license\"] is missing into {}".format(meta_file_path)
     return_code = 2
 
   try:
     meta["galaxy_info"]["min_ansible_version"]
     if not meta["galaxy_info"]["min_ansible_version"]:
-      print "the key [\"galaxy_info\"][\"min_ansible_version\"] is empty into meta/main.yml"
+      print "the key [\"galaxy_info\"][\"min_ansible_version\"] is empty into {}".format(meta_file_path)
       return_code = 2
   except KeyError as e:
-    print "the key [\"galaxy_info\"][\"min_ansible_version\"] is missing into meta/main.yml"
+    print "the key [\"galaxy_info\"][\"min_ansible_version\"] is missing into {}".format(meta_file_path)
     return_code = 2
 
   return return_code
@@ -135,26 +134,27 @@ def check_defaults_main():
   global return_code
   global role_path
   global role_name
-
+ 
+  default_main_path = "{}/defaults/main.yml".format(role_path) 
   try:
-    default_main=yaml_load("{}/defaults/main.yml".format(role_path))    
+    default_main=yaml_load(default_main_path)    
   except IOError as e:
-    print "FATAL: can't open the file {}/defaults/main.yml , this file is required for playbook run.".format(role_path)
+    print "FATAL: can't open the file , this file is required for playbook run.".format(default_main_path)
     print "test end  before the end"
     print "Now, i'am sad :("
     sys.exit(2)
 
   # default/main.yml  check naming convention
   if default_main is None:
-    print "file {}/defaults/main.yml doesn't have any variables".format(role_path)
+    print "file {} doesn't have any variables".format(default_main_path)
     return_code = 2
   else:
     for var_name in default_main:
      if re.match("^{}.*".format(role_name), var_name) is None:
-       print "{} propertie dont respect the naming convention prefix {}_ into {}/defaults/main.yml".format(
+       print "{} propertie dont respect the naming convention prefix {}_ into {}.".format(
          var_name, 
          role_name, 
-         role_path
+         default_main_path
        ) 
        return_code =2
 
@@ -165,17 +165,17 @@ def check_tasks_main():
   global role_path
   global role_name
 
-  #tasks/main.yml check tags convention
+  file_task_main_path = "{}/tasks/main.yml".format(role_path)
   try:
-    tasks_main=yaml_load("{}/tasks/main.yml".format(role_path))
+    tasks_main=yaml_load(file_task_main_path)
   except IOError as e:
-    print "FATAL: can't open the file {}/tasks/main.yml, this file is required for playbook run.".format(role_path)
+    print "FATAL: can't open the file {}, this file is required for playbook run.".format(file_task_main_path)
     print "test end  before the end"
     print "Now, i'am sad :("
     sys.exit(2)
   
   if tasks_main is None:
-    print "file {}/tasks/main.yml doesn't have any entries".format(role_path)
+    print "file {} doesn't have any entries".format(file_task_main_path)
     return_code = 2
   else:
     for entrie in tasks_main:
@@ -183,32 +183,38 @@ def check_tasks_main():
 
       try:
         if entrie["tags"][0] != role_name :
-          print "first tag for include {} should be {}, get {} instead into task/main.yml".format(
+          print "first tag for include {} should be {}, get {} instead into {}".format(
             include_name,
             role_name,
-            entrie["tags"][0])
+            entrie["tags"][0],
+            file_task_main_path
+          )
       except IndexError as e:
-        print  "tag {} is missing on include {} into task/main.yml".format(role_name,include_name)
+        print  "tag {} is missing on include {} into {}".format(role_name, include_name, file_task_main_path)
 
       try:
         if entrie["tags"][1] != include_name :
-          print "second tag for include {} should be {}, get {} instead into task/main.yml".format(
+          print "second tag for include {} should be {}, get {} instead into {}".format(
             include_name,
             include_name,
-            entrie["tags"][1])
+            entrie["tags"][1],
+            file_task_main_path
+          )
       except IndexError as e:
-        print  "tag {} is missing on include {} into task/main.yml".format(include_name,include_name)
+        print  "tag {} is missing on include {} into {}".format(include_name, include_name, file_task_main_path)
 
       try:
         tag3= "{}-{}".format(role_name,include_name)
         if entrie["tags"][2] != tag3 :
-          print "third tag for include {} should be {}, get {} instead into task/main.yml".format(
+          print "third tag for include {} should be {}, get {} instead into {}.".format(
             include_name,
             tag3,
-            entrie["tags"][2])
+            entrie["tags"][2],
+            file_task_main_path
+          )
 
       except IndexError as e:
-        print  "tag {} is missing on include {} into task/main.yml".format(include_name,include_name)
+        print  "tag {} is missing on include {} into {}.".format(include_name, include_name, file_task_main_path)
         return_code = 2
 
 
@@ -221,7 +227,7 @@ def check_templates():
        full_template_path="{}/templates/{}".format(role_path, template_filename)
 
        if not template_filename.endswith(".j2"):
-         print "file {} in folder template doesn't have the extension j2".format(template_filename)
+         print "file {} in folder template doesn't have the extension j2".format(full_template_path)
          return_code = 2
 
        try:
