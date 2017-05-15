dba66939f6dbf30dbd4de3a51c9cf7da1eb2aec2
Jonathan CHARDON <jonathan@skale-5.com>
2017-05-15 17:09:34 +0000

    update test.sh (change script naming)

--- test.sh
+++ test.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 echo -e "TEST:no_defaults\n"
-./ansible_check.py -p test/no_defaults
+./role_check.py -p test/no_defaults
 
 echo -e "\n\n\nTEST:no_task\n"
-./ansible_check.py -p test/no_tasks
+./role_check.py -p test/no_tasks
 
 echo -e "\n\n\nTEST:no_task_install\n"
-./ansible_check.py -p test/no_tasks_install
+./role_check.py -p test/no_tasks_install
 
 echo -e "\n\n\nTEST:no meta"
-./ansible_check.py -p test/no_meta
+./role_check.py -p test/no_meta
 
 echo -e "\n\n\nTEST:meta no galaxy-tags"
-./ansible_check.py -p test/meta_no_galaxy-tag
+./role_check.py -p test/meta_no_galaxy-tag
 
 echo -e "\n\n\nTEST:meta galaxy-tags empty"
-./ansible_check.py -p test/meta_no_galaxy-tag
+./role_check.py -p test/meta_no_galaxy-tag
 
 echo -e "\n\n\nTEST:meta author empty"
-./ansible_check.py -p test/meta_author_empty
+./role_check.py -p test/meta_author_empty
 
 echo -e "\n\n\nTEST:meta no author"
-./ansible_check.py -p test/meta_no_author
+./role_check.py -p test/meta_no_author
 
 echo -e "\n\n\nTEST:meta description empty"
-./ansible_check.py -p test/meta_description_empty
+./role_check.py -p test/meta_description_empty
 
 echo -e "\n\n\nTEST:meta no description"
-./ansible_check.py -p test/meta_no_description
+./role_check.py -p test/meta_no_description
 
 echo -e "\n\n\nTEST:meta platforms empty"
-./ansible_check.py -p test/meta_platforms_empty
+./role_check.py -p test/meta_platforms_empty
 
 echo -e "\n\n\nTEST:meta no platforms"
-./ansible_check.py -p test/meta_no_platforms
+./role_check.py -p test/meta_no_platforms
 
 echo -e "\n\n\nTEST:template file empty"
-./ansible_check.py -p test/template_empty
+./role_check.py -p test/template_empty
 
 echo -e "\n\n\nTEST:template file correct but no j2 extension"
-./ansible_check.py -p test/template_not_j2
+./role_check.py -p test/template_not_j2
 
 echo -e "\n\n\nTEST:template file correct but no templated variable on it"
-./ansible_check.py -p test/template_not_templated_var
+./role_check.py -p test/template_not_templated_var
 
